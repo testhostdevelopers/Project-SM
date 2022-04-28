65fcb6f54f56ddd822ba40b948bad70d46706a67
vishal jariwala <vishalj.kamaldhari@gmail.com>
2022-04-28 12:34:03 +0530

    334 - Display names

--- js/packages/web/src/components/ArtistCard/index.tsx
+++ js/packages/web/src/components/ArtistCard/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import { Card } from 'antd';
 
-import { Artist } from '../../types';
-
-import { shortenAddress } from '@oyster/common';
-import { MetaAvatar } from '../MetaAvatar';
+// import { Artist } from '../../types';
+// import { shortenAddress } from '@oyster/common';
+// import { MetaAvatar } from '../MetaAvatar';
 
 export const ArtistCard = (props: any) => {
-  let { artistDetail , artist} = props
-  
-  let creatorDeatil = (artistDetail.prismicContent && artistDetail.prismicContent.length > 0 && artistDetail.prismicContent[0].data.creator.length > 0) && artistDetail.prismicContent[0].data.creator.filter((x) => (x.creator_id[0].text === artist.address));
+  const { artistDetail, artist } = props;
+
+  const creatorDeatil =
+    artistDetail.prismicContent &&
+    artistDetail.prismicContent.length > 0 &&
+    artistDetail.prismicContent[0].data.creator.length > 0 &&
+    artistDetail.prismicContent[0].data.creator.filter(
+      x => x.creator_id[0].text === artist.address,
+    );
 
   return (
     <Card
@@ -17,7 +22,11 @@ export const ArtistCard = (props: any) => {
       className={`artist-card`}
       cover={
         <div className="header-container">
-          {(creatorDeatil && creatorDeatil.length > 0) ? <img src={creatorDeatil[0].profile_pic.url} /> : <img src="/profile-img.png" />}
+          {creatorDeatil && creatorDeatil.length > 0 ? (
+            <img src={creatorDeatil[0].profile_pic.url} />
+          ) : (
+            <img src="/profile-img.png" />
+          )}
         </div>
       }
       bordered={false}
@@ -25,7 +34,9 @@ export const ArtistCard = (props: any) => {
       <>
         {/* <MetaAvatar creators={[artist]} size={64} /> */}
         <div className="artist-card-name">
-          {props.artist?.name || shortenAddress(props.artist?.address || '')}
+          {creatorDeatil[0].creator_name[0].text ||
+            creatorDeatil[0].creator_name[0].text ||
+            creatorDeatil[0].creator_id[0].text}
         </div>
         <div className="artist-card-description">{props.artist?.about}</div>
       </>
