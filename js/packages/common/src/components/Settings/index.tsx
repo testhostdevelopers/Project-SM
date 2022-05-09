cd52d2a9b9dd2982db6d8f6ad097138d35075171
ma06rii <ma06rii@live.co.uk>
2022-05-09 09:48:41 +0100

    add temporary solution to get creator details

--- js/packages/common/src/components/Settings/index.tsx
+++ js/packages/common/src/components/Settings/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Tooltip } from 'antd';
 import { useWallet } from '@solana/wallet-adapter-react';
 import { shortenAddress } from '../../utils';
@@ -6,18 +6,47 @@ import { CopyOutlined } from '@ant-design/icons';
 // import { Identicon } from '../Identicon';
 // import { Link } from 'react-router-dom';
 // import { useMeta } from '@oyster/common';
+import Prismic from 'prismic-javascript';
 
 export const Settings = (props: any) => {
   const { publicKey } = useWallet();
   // const { whitelistedCreatorsByCreator } = useMeta();
-  const { prismicContent } = props.userDetail.children._owner.memoizedProps;
-  const creatorDeatil =
-    prismicContent &&
-    prismicContent.length > 0 &&
-    prismicContent[0].data.creator.length > 0 &&
-    prismicContent[0].data.creator.filter(
-      (x: any) => x.creator_id[0].text === publicKey?.toBase58(),
+  // const { prismicContent } = props.userDetail.children._owner.memoizedProps;
+  // const creatorDeatil =
+  //   prismicContent &&
+  //   prismicContent.length > 0 &&
+  //   prismicContent[0].data.creator.length > 0 &&
+  //   prismicContent[0].data.creator.filter(
+  //     (x: any) => x.creator_id[0].text === publicKey?.toBase58(),
+  //   );
+
+  const [creatorDetail, setCreatorDetail] = useState<any>();
+
+  useEffect(() => {
+    const client = Prismic.client(
+      'https://starlight-metaplex.prismic.io/api/v2',
     );
+    client
+      .query([Prismic.Predicates.at('document.type', 'test')])
+      .then(res => {
+        const _myPrismicContent = res.results;
+
+        const creatorDetail =
+          _myPrismicContent &&
+          _myPrismicContent.length > 0 &&
+          _myPrismicContent[0].data.creator.length > 0 &&
+          _myPrismicContent[0].data.creator.filter(
+            (x: any) => x.creator_id[0].text === publicKey?.toBase58(),
+          );
+
+        setCreatorDetail(creatorDetail);
+
+        console.log('internal_response:::---', res);
+      })
+      .catch(err => {
+        console.log('err is ', err);
+      });
+  });
 
   return (
     <>
@@ -37,8 +66,8 @@ export const Settings = (props: any) => {
         /> */}
 
         <div className="drop-profile-img">
-          {creatorDeatil && creatorDeatil.length > 0 ? (
-            <img src={creatorDeatil[0].profile_pic.url} />
+          {creatorDetail && creatorDetail.length > 0 ? (
+            <img src={creatorDetail[0].profile_pic.url} />
           ) : (
             <img src="/profile-img.png" />
           )}
